add0167dfbbf81f956d3c6384d1bae2510f55653
Gchennareddy <guvvalachennareddy@gmail.com>
2018-07-26 13:21:53 +0530

    second program

---
+++ file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+PLANETS=('mercury','venus','earth','jupiter')
+for planets in PLANETS:
+  if (planets is 'jupiter'):
+     print "we are land on jupiter"
+     break
+  else:
+    print "loop terminates"
